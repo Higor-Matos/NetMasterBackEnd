a9c32cca4432a38dd64d0ea780d765e3f835f49f
Higor de Deus <higordeus22@gmail.com>
2023-03-25 00:37:42 -0300

    Refatoração da camada Presentation

---
+++ NetMaster.Presentation/Startup.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using NetMaster.Presentation;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerDocumentation();
+
+var app = builder.Build();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseSwaggerDocumentation();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.Run();
