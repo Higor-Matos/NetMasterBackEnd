613cb657468cef00ae5a9ebb956e7865d817ebd7
Higor de Deus <higordeus22@gmail.com>
2023-03-25 01:49:19 -0300

    Correção, agora envio scripts e não comandos powershell

--- NetMaster.Repository/Local/Powershell/BasePowershellRepository.cs
+++ NetMaster.Repository/Local/Powershell/BasePowershellRepository.cs
@@ -1,5 +1,6 @@
 ﻿using NetMaster.Domain.Models;
 using NetMaster.Domain.Models.Results;
+using NetMaster.Repository.Local.Powershell;
 using System.Management.Automation;
 using System.Management.Automation.Runspaces;
 
@@ -14,56 +15,55 @@ public abstract class BasePowershellRepository
 
     public abstract Task<RepositoryResultModel> ExecCommand(RepositoryPowerShellParamModel param);
 
-        protected async Task<RepositoryResultModel> RunCommand(
-            RepositoryPowerShellParamModel param,
-            string commandPowershell,
-            string? commandParameterPowershell = null
-        )
+    protected async Task<RepositoryResultModel> RunCommand(
+        RepositoryPowerShellParamModel param,
+        string commandPowershell,
+        string? commandParameterPowershell = null
+    )
+    {
+        PSCredential credential = credentialProvider.GetCredential();
+        WSManConnectionInfo wsManConnectionInfo = new()
         {
-            PSCredential credential = credentialProvider.GetCredential();
-            WSManConnectionInfo wsManConnectionInfo = new()
-            {
-                ComputerName = param.Ip,
-                Credential = credential
-            };
+            ComputerName = param.Ip,
+            Credential = credential
+        };
 
-            return await RunCommandInSpace(wsManConnectionInfo, commandPowershell, commandParameterPowershell);
+        return await RunCommandInSpace(wsManConnectionInfo, commandPowershell, commandParameterPowershell);
+    }
+
+    private static async Task<RepositoryResultModel> RunCommandInSpace(
+        WSManConnectionInfo wsManConnectionInfo,
+        string command,
+        string? parameters
+    )
+    {
+        using Runspace runSpace = RunspaceFactory.CreateRunspace(wsManConnectionInfo);
+        try
+        {
+            using PowerShell powerShell = PowerShell.Create();
+
+            runSpace.Open();
+            powerShell.Runspace = runSpace;
+
+            powerShell.AddScript(command);
+
+            if (!string.IsNullOrEmpty(parameters))
+            {
+                powerShell.AddParameter(parameters + "\n");
+            }
+
+            var commandResult = await powerShell.InvokeAsync();
+            var returnResult = string.Join(Environment.NewLine, commandResult);
+
+            return new RepositoryResultModel(success: new SuccessRepositoryResult(returnResult));
         }
-
-        private static async Task<RepositoryResultModel> RunCommandInSpace(
-            WSManConnectionInfo wsManConnectionInfo,
-            string command,
-            string? parameters
-        )
+        catch (Exception e)
         {
-            using Runspace runSpace = RunspaceFactory.CreateRunspace(wsManConnectionInfo);
-            try
-            {
-                using PowerShell powerShell = PowerShell.Create();
-
-                runSpace.Open();
-                powerShell.Runspace = runSpace;
-
-                powerShell.AddCommand(command);
-
-                if (!string.IsNullOrEmpty(parameters))
-                {
-                    powerShell.AddParameter(parameters + "\n");
-                }
-
-                var commandResult = await powerShell.InvokeAsync();
-                var returnResult = string.Join(Environment.NewLine, commandResult);
-
-                return new RepositoryResultModel(success: new SuccessRepositoryResult(returnResult));
-            }
-            catch (Exception e)
-            {
-                return new RepositoryResultModel(error: new ErrorRepositoryResult(e));
-            }
-            finally
-            {
-                runSpace.Close();
-            }
+            return new RepositoryResultModel(error: new ErrorRepositoryResult(e));
+        }
+        finally
+        {
+            runSpace.Close();
         }
     }
 }
