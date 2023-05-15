84c9e61d4400c6fb890fdf8aaba98ef9828f076e
Higor de Deus <higordeus22@gmail.com>
2023-05-15 11:39:36 -0300

    Padronização das interfaces de hardware, tudo OK

--- NetMaster.Services.csproj/Hardware/StorageInfoService.cs
+++ NetMaster.Services.csproj/Hardware/StorageInfoService.cs
@@ -18,20 +18,32 @@ namespace NetMaster.Services.Hardware
             _localStorageRepository = localStorageRepository;
         }
 
-        public async Task SaveLocalStorageInfoAsync(string ip)
+        public async Task<ServiceResultModel<StorageInfoDataModel>> SaveLocalStorageInfoAsync(string ip)
         {
             RepositoryResultModel<StorageInfoDataModel> localStorageInfoResult = await _localStorageRepository.ExecCommand(new RepositoryPowerShellParamModel(ip));
             if (localStorageInfoResult.SuccessResult != null)
             {
                 await _storageRepository.InsertAsync(localStorageInfoResult.SuccessResult.Result);
+                SuccessServiceResult<StorageInfoDataModel> successResult = new(localStorageInfoResult.SuccessResult.Result, DateTime.UtcNow, ip);
+                return new ServiceResultModel<StorageInfoDataModel>(successResult);
+            }
+            else if (localStorageInfoResult.ErrorResult != null)
+            {
+                ErrorServiceResult errorResult = new(localStorageInfoResult.ErrorResult.Exception.Message, DateTime.UtcNow, ip);
+                return new ServiceResultModel<StorageInfoDataModel>(error: errorResult);
+            }
+            else
+            {
+                ErrorServiceResult errorResult = new($"Failed to get local storage info.", DateTime.UtcNow, ip);
+                return new ServiceResultModel<StorageInfoDataModel>(error: errorResult);
             }
         }
+
         public async Task<ServiceResultModel<StorageInfoDataModel>> GetStorageInfoByComputerNameAsync(string computerName)
         {
             StorageInfoDataModel storageInfo = await _storageRepository.GetByComputerNameAsync(computerName);
             SuccessServiceResult<StorageInfoDataModel> successResult = new(storageInfo, DateTime.UtcNow, computerName);
             return new ServiceResultModel<StorageInfoDataModel>(successResult);
         }
-
     }
 }
--- NetMaster.Services.csproj/Interfaces/IStorageInfoService.cs
+++ NetMaster.Services.csproj/Interfaces/IStorageInfoService.cs
@@ -1,13 +1,12 @@
 ﻿// NetMaster.Services/Interfaces/IStorageInfoService.cs
 using NetMaster.Domain.Models.DataModels;
 using NetMaster.Domain.Models.Results;
-using System.Threading.Tasks;
 
 namespace NetMaster.Services.Interfaces
 {
     public interface IStorageInfoService
     {
-        Task SaveLocalStorageInfoAsync(string ip);
+        Task<ServiceResultModel<StorageInfoDataModel>> SaveLocalStorageInfoAsync(string ip);
         Task<ServiceResultModel<StorageInfoDataModel>> GetStorageInfoByComputerNameAsync(string computerName);
     }
 }
